7ea69380b856efef3483148035d15f1313554d2e
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-04-23 22:44:00 +0200

    ajout fonctions écriture de QR codes et gitignore

--- projet_lecture_qr_code.py
+++ projet_lecture_qr_code.py
@@ -6,9 +6,11 @@ from PIL import ImageTk
 from tkinter import filedialog
 import tkinter.messagebox as mb
 
+create=True
+
 #Fonctions
 def nbrCol(mat):
-    return(len(mat[0]))
+    return len(mat[0])
 
 def nbrLig(mat):
     return len(mat)
@@ -29,6 +31,31 @@ def loading(filename):#charge le fichier image filename et renvoie une matrice d
         for j in range(toLoad.size[0]):
             mat[i][j]= 0 if toLoad.getpixel((j,i)) == 0 else 1
     return mat
+
+def zoom(mat):
+    mat_zoom = [[0 for i in range (nbrCol(mat) * 2)] for j in range (nbrLig(mat) * 2)]
+    #créer une matrice de largeur et hauteur deux fois plus grande 
+    for i in range(nbrLig(mat)):
+        for j in range(nbrCol(mat)):
+            mat_zoom[2*i][2*j] = mat[i][j]
+            mat_zoom[2*i+1][2*j] = mat[i][j]
+            mat_zoom[2*i][2*j+1] = mat[i][j]
+            mat_zoom[2*i+1][2*j+1] = mat[i][j]
+    return mat_zoom
+
+def charger(filename):
+    global create
+    img = pil.Image.open(filename)
+    photo = ImageTk.PhotoImage(img)
+    if create:    
+        canvas = tk.Canvas(racine, width = img.size[0], height = img.size[1])
+        dessin = canvas.create_image(0,0,anchor = tk.NW, image=photo)
+        canvas.grid(row=4,column=2,rowspan=12)
+        create=False
+    else:
+        canvas = tk.Canvas(racine, width = img.size[0], height = img.size[1])
+        dessin=canvas.create_image(0,0,anchor = tk.NW, image=photo)
+        canvas.grid(row=4,column=2,rowspan=12)
 
 def rotate(mat):
     mat_rota = [[0 for i in range (nbrLig(mat))] for j in range (nbrCol(mat))]
@@ -56,7 +83,7 @@ def verif_coin(QR):
                 QR = rotate(QR)
             return(QR)
         cpt += 1
-    return(QR)
+    return QR
 
 def verif_ligne_colonne(QR):
     l1 = [i % 2 for i in range (13)] 
@@ -73,7 +100,7 @@ def verif_ligne_colonne(QR):
         return False
     return True
 
-def decode_Hamming74 (bits):
+def decode_Hamming74(bits):
     c1 = bits[4] != (bits[0] + bits[1] + bits[3])%2
     c2 = bits[5] != (bits[0] + bits[2] + bits[3])%2
     c3 = bits[6] != (bits[1] + bits[2] + bits[3])%2
@@ -87,13 +114,13 @@ def decode_Hamming74 (bits):
         bits[2] = (bits[2] + 1) % 2
     return [bits[0], bits[1], bits[2], bits[3]]
 
-def code_Hamming74 (bits):
+def code_Hamming74(bits):
     v1 = (bits[0] + bits[1] + bits[3])%2
     v2 = (bits[0] + bits[2] + bits[3])%2
     v3 = (bits[1] + bits[2] + bits[3])%2
     return [bits[0], bits[1], bits[2], bits[3], v1, v2, v3]
 
-def lecture (QR):
+def lecture(QR):
     bloc = []
     L_QR = []
     type_donnees = QR[24][8]
@@ -120,7 +147,7 @@ def lecture (QR):
                     return L_QR, type_donnees
     return L_QR, type_donnees
 
-def decodage (LQR_et_type):
+def decodage(LQR_et_type):
     L_QR = LQR_et_type[0]
     type_donnees = LQR_et_type[1]
     txt = ""
@@ -152,18 +179,113 @@ def enlever_filtre(QR):
             QR[i][j] = QR[i][j]^filtre[i][j]
     return QR
 
-def lire():
+def lire ():
     filename = filedialog.askopenfile(mode='rb', title='Choose a file')
     QR = verif_coin(loading(filename))
     if verif_ligne_colonne(QR) :
-        label_lu.config(text = decodage(lecture(enlever_filtre(QR))))
+        l_contenu.config(text = "Contenu du QR code : " + decodage(lecture(enlever_filtre(QR))))
+
+def encodage(msg):
+    L_QR = []
+    for x in msg:
+        if v_type.get() == "1":
+            c = format(ord(x), "b")
+            for n in range(8 - len(c)):
+                c = "0" + c
+            L_QR.extend(code_Hamming74(list(map(int, c))[:4]) + code_Hamming74(list(map(int, c))[4:]))
+        else:
+            c = format(int(x,16), "b")
+            for n in range(4 - len(c)):
+                c = "0" + c
+            L_QR.extend(code_Hamming74(list(map(int, c))))
+    return L_QR
+
+def ecriture_donnes (QR, msg):
+    QR[24][8] = int(v_type.get())
+    nbr_bloc_bin = list(map(int, format(len(msg), "b")))
+    for n in range(5 - len(nbr_bloc_bin)):
+                nbr_bloc_bin = [0] + nbr_bloc_bin
+    for n in range (13, 18):
+        QR[n][0] = nbr_bloc_bin[n - 13]
+    QR[22][8] = int(v_filtre.get()[1])
+    QR[23][8] = int(v_filtre.get()[4])
+    return QR
+
+def ecriture_msg(L_QR, QR):
+    cpt = 0
+    for i in range (24, 8, -2):
+        if i % 4 == 0:
+            m = 24
+            n = 10
+            p = -1
+        else :
+            m = 11
+            n = 25
+            p = 1
+        for j in range (m, n, p):
+            QR[i][j] = L_QR[cpt]
+            QR[i-1][j] = L_QR[cpt+1]
+            cpt += 2
+            if cpt >= len(L_QR):
+                return QR
+    return QR
+
+def ecrire ():
+    msg = e_msg.get()
+    if len(msg) > 16:
+        mb.showerror("Erreur", "La taille maximum du message est 16 caractères")
+        return
+    if v_type.get() == "0" and len(msg)%2 == 1:
+        mb.showerror("Erreur", "Un message en hexadécimal doit avoir une taille paire")
+        return
+    QR = ecriture_donnes(loading("frame.png"), msg)
+    QR = ecriture_msg(encodage(msg), QR)
+    saving(QR, "qr_code_genere_" + e_nom.get() + ".png")
+    saving(zoom(QR), "qr_code_genere_zoome")
+    charger("qr_code_genere_zoome")
 
 racine=tk.Tk()
+racine.title("Lecture et écriture de QR codes")
+l_titre_lecture = tk.Label(racine, text="Lecture de QR codes")
+b_lire=tk.Button(racine, text="Lire un QR code", command = lire)
+l_contenu = tk.Label(racine, text="Contenu du QR code : ")
+l_separation = tk.Label(racine, text="")
+l_titre_ecriture = tk.Label(racine, text="Ecriture de QR codes")
+l_info_ecriture = tk.Label(racine, text="Informations à écrire dans le QR code : ")
+e_msg = tk.Entry(racine)
+l_type = tk.Label(racine, text="Format des données :")
+v_type = tk.StringVar()
+v_type.set(1)
+rb_ascii = tk.Radiobutton(racine, variable=v_type, text="Ascii", value=1)
+rb_hexa = tk.Radiobutton(racine, variable=v_type, text="Hexadécimal", value=0)
+l_filtre = tk.Label(racine, text="Filtre :")
+v_filtre = tk.StringVar()
+v_filtre.set((0, 0))
+rb_aucun = tk.Radiobutton(racine, variable=v_filtre, text="Pas de filtre", value=(0, 0))
+rb_damier = tk.Radiobutton(racine, variable=v_filtre, text="Damier", value=(0, 1))
+rb_horizontal = tk.Radiobutton(racine, variable=v_filtre, text="Lignes horizontales alternées", value=(1, 0))
+rb_vertical = tk.Radiobutton(racine, variable=v_filtre, text="Lignes verticales alternées", value=(1, 1))
+l_nom = tk.Label(racine, text="Nom du QR code : ")
+e_nom = tk.Entry(racine)
+b_ecrire=tk.Button(racine, text="Créer le QR code", command = ecrire)
 
-b_lire=tk.Button(racine, text="Lire un QR code", command = lire, width = 100)
-label_lu = tk.Label(racine, width = 100)
-
-b_lire.pack(side="top", fill="x")
-label_lu.pack(side = "bottom", fill = "x")
+l_titre_lecture.grid(columnspan=2)
+b_lire.grid(row=1, sticky="nsw")
+l_contenu.grid(row=2, sticky="nsw")
+l_separation.grid(row=3)
+l_titre_ecriture.grid(row=4, columnspan=2)
+l_info_ecriture.grid(row=5, sticky="nsw")
+e_msg.grid(row=5, column=1, sticky="nsw")
+l_type.grid(row=6, sticky="nsw")
+rb_ascii.grid(row=7, sticky="nsw", ipadx = 10)
+rb_hexa.grid(row=8, sticky="nsw", ipadx = 10)
+l_filtre.grid(row=9, sticky="nsw")
+rb_aucun.grid(row=10, sticky="nsw", ipadx = 10)
+rb_damier.grid(row=11, sticky="nsw", ipadx = 10)
+rb_horizontal.grid(row=12, sticky="nsw", ipadx = 10)
+rb_vertical.grid(row=13, sticky="nsw", ipadx = 10)
+l_nom.grid(row=14, sticky="nsw")
+e_nom.grid(row=14, column=1, sticky="nsw")
+b_ecrire.grid(row=15, sticky="nsw")
 
 racine.mainloop()
